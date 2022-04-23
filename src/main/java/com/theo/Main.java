62e9ee56665ac62bcf2680e0faf31543f9290d33
TheophilusM <theotakumug@gmail.com>
2022-04-23 12:08:25 +0200

    syntax

--- src/main/java/com/theo/Main.java
+++ src/main/java/com/theo/Main.java
@@ -2,6 +2,32 @@ package com.theo;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(args[0]);    // Passed as >> java Main 23 "Theo"
+        System.out.println("Data Structures and Algorithm");
     }
 }
+
+// public - accessible from anywhere
+// Main - file and class name
+// main method
+// static - make object be executed without creating an object of the class
+// void - return type of the method
+// String[] args - array of arguments, args - array name
+// System.out.println()
+/*
+    System:
+        - class for printing data
+    out:
+        - class of PrintStream
+        - default command line
+    println:
+        - outputs a string
+        - adds ne line after printing the string
+ */
+
+/*
+    Compilation:
+        >> javac Main.java
+        >> javac -d .. Main.java    // specifying location to save
+        >> java Main
+ */
