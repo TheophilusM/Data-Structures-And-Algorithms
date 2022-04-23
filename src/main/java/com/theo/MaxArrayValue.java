acd5ab3b3846a5f540cfaed7203373fa7a5a99a2
TheophilusM <theotakumug@gmail.com>
2022-04-24 00:05:35 +0200

    find largest value

---
+++ src/main/java/com/theo/MaxArrayValue.java
@@ -0,0 +1,21 @@
+package com.theo;
+
+import java.util.Arrays;
+
+public class MaxArrayValue {
+    public static void main(String[] args) {
+        int[] ints = {1, 32, 24, 43, 5, 4, 54 ,6, 4};
+        swap(ints);
+    }
+
+    static void swap(int[] ints) {
+        int largest = 0;
+        for (int i: ints
+             ) {
+            if(i > largest) {
+                largest = i;
+            }
+        }
+        System.out.println("Largest value: " + largest);
+    }
+}
--- src/main/java/com/theo/SwapArrayValues.java
+++ src/main/java/com/theo/SwapArrayValues.java
@@ -1,7 +1,17 @@
 package com.theo;
 
+import java.util.Arrays;
+
 public class SwapArrayValues {
     public static void main(String[] args) {
-        int[] ints = {1, 32, 24, 43, 5};
+        int[] ints = {1, 32, 24, 43, 5, 4, 54 ,6};
+        swap(ints, 2, 4);
+    }
+
+    static void swap(int[] ints, int index1, int index2) {
+        int temp  = ints[index1];
+        ints[index1] = ints[index2];
+        ints[index2] = temp;
+        System.out.println(Arrays.toString(ints));
     }
 }
