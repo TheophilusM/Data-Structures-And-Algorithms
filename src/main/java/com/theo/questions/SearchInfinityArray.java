4928726dc54170deb6442448f33b22a716a49dd1
TheophilusM <theotakumug@gmail.com>
2022-04-24 17:40:15 +0200

    SearchInfinityArray

---
+++ src/main/java/com/theo/questions/SearchInfinityArray.java
@@ -0,0 +1,45 @@
+package com.theo.questions;
+
+// Find target in a sorted array of infinity size of array
+public class SearchInfinityArray {
+    public static void main(String[] args) {
+        int[] ints = {2, 2, 3, 5, 5, 9, 9, 9, 14, 14, 16, 16, 18};
+        int target = 9;
+        System.out.println("Result: " +findStartAndEnd(ints, target) );
+    }
+
+    static int findStartAndEnd(int[] arr, int target) {
+        // start with window if 2
+        int start = 0;
+        int end = 2;
+
+        // condition of target in range
+        while (true) {
+            if (target < end) {
+                return binarySearch(arr, target, start, end);
+            } else {
+                start = end + 1;
+                end = end*end;
+            }
+        }
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            // find the middle element
+            // int mid = (start + end) / 2;     // int has a fixed size range ! ! !
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+        }
+
+        // if value not found
+        return -1;
+    }
+}
